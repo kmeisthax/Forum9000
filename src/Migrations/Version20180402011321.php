8b1d844def209c644ca6d013b23479380d71f8ba
David Wendt <dcrkid@yahoo.com>
2018-04-02 18:52:36 -0400

    Port migrations over to MySQL so that we can run them on both sqlite and mysql.

--- src/Migrations/Version20180402011321.php
+++ src/Migrations/Version20180402011321.php
@@ -12,22 +12,26 @@ class Version20180402011321 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
-        $this->addSql('CREATE TABLE post (id CHAR(36) NOT NULL --(DC2Type:guid)
-        , thread_id CHAR(36) DEFAULT NULL --(DC2Type:guid)
-        , title VARCHAR(255) NOT NULL, message CLOB NOT NULL, et_order INTEGER NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_5A8A6C8DE2904019 ON post (thread_id)');
-        $this->addSql('CREATE TABLE thread (id CHAR(36) NOT NULL --(DC2Type:guid)
-        , PRIMARY KEY(id))');
+        switch ($this->connection->getDatabasePlatform()->getName()) {
+            case "sqlite":
+                $this->addSql('CREATE TABLE post (id CHAR(36) NOT NULL --(DC2Type:guid)
+            , thread_id CHAR(36) DEFAULT NULL --(DC2Type:guid)
+            , title VARCHAR(255) NOT NULL, message CLOB NOT NULL, et_order INTEGER NOT NULL, PRIMARY KEY(id))');
+                $this->addSql('CREATE INDEX IDX_5A8A6C8DE2904019 ON post (thread_id)');
+                $this->addSql('CREATE TABLE thread (id CHAR(36) NOT NULL --(DC2Type:guid)
+                , PRIMARY KEY(id))');
+                break;
+            case "mysql":
+                $this->addSql('CREATE TABLE post (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', thread_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', title VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, et_order INT NOT NULL, INDEX IDX_5A8A6C8DE2904019 (thread_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+                $this->addSql('CREATE TABLE thread (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+                break;
+            default:
+                $this->abort('Migration can only be executed safely on \'sqlite\' or \'mysql\'.');
+                break;
+        }
     }
 
-    public function down(Schema $schema)
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
+    public function down(Schema $schema) {
         $this->addSql('DROP TABLE post');
         $this->addSql('DROP TABLE thread');
     }
